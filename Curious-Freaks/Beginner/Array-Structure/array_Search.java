1339bfeef1597251eef9d8112aacbcb151b61d37
Kuru07 <kumarakuru7007@gmail.com>
2024-12-08 21:14:40 +0530

    Curious

---
+++ Curious-Freaks/Beginner/Array-Structure/array_Search.java
@@ -0,0 +1,14 @@
+//Array search
+class Solution {
+
+    static int search(int arr[], int x) {
+
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==x)
+            return i;
+        }
+        return -1;
+    }
+}
